0dd11763288900dd73d969a7ca0b46b2ff3b0504
homola.p <homola.p@d4bbbbe0-13e3-11df-ba53-e97ec090ad2f>
2010-02-07 14:21:39 +0000

    first beta

---
+++ trunk/src/service/CBStringService.h
@@ -0,0 +1,16 @@
+//
+//  CBStringService.h
+//  Chibcha
+//
+//  Created by PH on 7.2.10.
+//  Copyright 2010 Codesign. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CBService.h"
+
+@interface CBStringService : CBService {
+
+}
+
+@end
---
+++ trunk/src/service/CBStringService.m
@@ -0,0 +1,28 @@
+//
+//  CBStringService.h
+//  Chibcha
+//
+//  Created by PH on 7.2.10.
+//  Copyright 2010 Codesign. All rights reserved.
+//
+
+#import "CBStringService.h"
+
+@implementation CBStringService
+
+- (id)init {
+	if ((self = [super init])) {
+		name = @"CBService:String";
+	}
+	return self;
+}
+
+- (NSData*)processRequestWithParameters:(NSDictionary*)parameters {
+	NSString* string = [parameters objectForKey: @"string"];
+	NSLog(@"Input string: %@", string);
+	NSString* strippedString = [[[NSString alloc] initWithData: [string dataUsingEncoding: NSASCIIStringEncoding allowLossyConversion: YES] encoding: NSASCIIStringEncoding] autorelease];
+	NSString* result = [NSString stringWithFormat: @"%@ -> %@", string, strippedString];
+	return [result dataUsingEncoding: NSUTF8StringEncoding];
+}
+
+@end
