3c8585d3749b82943383c4665fbcc3e812e3cac4
homola.p <homola.p@d4bbbbe0-13e3-11df-ba53-e97ec090ad2f>
2010-02-09 03:44:36 +0000

    sessions & timeouts

--- src/service/CBService.h
+++ src/service/CBService.h
@@ -7,16 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CBSession.h"
+
+@class CBServiceCenter;
 
 @interface CBService : NSObject {
 	NSString* name;
+	//CBServiceCenter* serviceCenter;
 }
 
 @property (nonatomic, retain) NSString* name;
+//@property (nonatomic, assign) CBServiceCenter* serviceCenter;
 
 - (NSDictionary*)parametersWithParamString:(NSString*)paramString;
-- (NSData*)processRequestWithParamString:(NSString*)paramString data:(NSData*)data sessionID:(NSString*)sessionID;
-- (NSData*)processRequestWithParameters:(NSDictionary*)parameters data:(NSData*)data sessionID:(NSString*)sessionID;
+- (NSData*)processRequestWithParamString:(NSString*)paramString data:(NSData*)data session:(CBSession*)session;
+- (NSData*)processRequestWithParameters:(NSDictionary*)parameters data:(NSData*)data session:(CBSession*)session;
 - (NSString*)MIMEType;
+- (BOOL)isThreadSafe;
+- (NSTimeInterval)timeout;
 
 @end
--- src/service/CBServiceCenter.m
+++ src/service/CBServiceCenter.m
@@ -8,17 +8,21 @@
 
 #import "CBServiceCenter.h"
 #import "CBService.h"
+#import "CBReturnValue.h"
 
 #define NUMBER_OF_THREADS	10
+#define SLEEP_INTERVAL		0.01
 
 @implementation CBServiceCenter
 
 @synthesize name;
 @synthesize services;
+@synthesize sessions;
 
 - (id)init {
 	if ((self = [super init])) {
 		services = [[NSMutableDictionary alloc] init];
+		sessions = [[NSMutableDictionary alloc] init];
 		//Class DBClass = NSClassFromString(@"EXObjectStore");
 		//if (DBClass == Nil) NSLog(@"No database (class not found)");
 	}
@@ -31,23 +35,71 @@
 	[service release];
 }
 
+- (void)discardSessionWithSessionID:(NSString*)sessionID {
+	//NSLog(@"Discarding session (exists: %d)", [sessions objectForKey: sessionID] != nil);
+	[sessions removeObjectForKey: sessionID];
+}
+
 - (NSMutableDictionary*)processRequestWithServiceName:(NSString*)serviceName paramString:(NSString*)paramString data:(NSData*)_data sessionID:(NSString*)sessionID {
-	CBService* service = [self.services objectForKey: serviceName];
-	if (service == nil) {
-		NSLog(@"Could not find service '%@'", serviceName);
-	} else {
-		NSString* MIMEType = [service MIMEType];
-		NSData* data = [service processRequestWithParamString: paramString data: _data sessionID: sessionID];
-		return [NSMutableDictionary dictionaryWithObjectsAndKeys: MIMEType, @"MIMEType", data, @"Data", nil];
+	@try {
+		CBService* service = [self.services objectForKey: serviceName]; // thread-safe thanks to "self." (it's not "nonatomic")
+		if (service == nil) {
+			NSLog(@"Could not find service '%@'", serviceName);
+		} else {
+			CBSession* session = [sessions objectForKey: sessionID];
+			if (session == nil) {
+				session = [[CBSession alloc] init];
+				[sessions setObject: session forKey: sessionID];
+				[session release];
+			}
+			NSString* MIMEType = [service MIMEType];
+			CBReturnValue* retVal = [[[CBReturnValue alloc] init] autorelease];
+			[NSThread detachNewThreadSelector: @selector(processBlock:)
+									 toTarget: self
+								   withObject: [[^void() {
+				if ([service isThreadSafe])
+					retVal.value = [service processRequestWithParamString: paramString data: _data session: session];
+				else @synchronized (service) {
+					retVal.value = [service processRequestWithParamString: paramString data: _data session: session];
+				}
+			} copy] autorelease]];
+			NSDate* expires = [NSDate dateWithTimeIntervalSinceNow: [service timeout]];
+			while (retVal.value == nil) {
+				if ([expires timeIntervalSinceNow] < 0) {
+					NSLog(@"Timeout: %@", serviceName);
+					break;
+				}
+				[NSThread sleepForTimeInterval: SLEEP_INTERVAL];
+			}
+			NSData* data = retVal.value;
+			return [NSMutableDictionary dictionaryWithObjectsAndKeys: MIMEType, @"MIMEType", data, @"Data", nil];
+		}
+	} @catch (NSException* exception) {
+		NSLog(@"Exception serving request: %@", exception);
 	}
 	return nil;
 }
 
+- (void)processBlock:(void(^)())block {
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+	@try {
+		block();
+	} @catch (NSException* exception) {
+		NSLog(@"Exception in block: %@", exception);
+	} @finally {
+		[pool drain];
+	}
+}
+
 - (BOOL)startServing {
 	id connection = [NSConnection serviceConnectionWithName: name rootObject: self];
+	[[NSNotificationCenter defaultCenter] addObserver: self
+											 selector: @selector(connectionDidDie:)
+												 name: NSConnectionDidDieNotification
+											   object: connection];
 	if (connection != nil) {
 		[connection enableMultipleThreads];
-		for (int i = 1; i < NUMBER_OF_THREADS; i++) {
+		for (int i = 1; i < [self numberOfThreads]; i++) {
 			[connection runInNewThread];
 		}
 		[connection run];
@@ -56,9 +108,18 @@
 	return connection != nil;
 }
 
+- (NSUInteger)numberOfThreads {
+	return NUMBER_OF_THREADS;
+}
+
+- (void)connectionDidDie:(NSNotification*)notofication {
+	NSLog(@"Connection died");
+}
+
 - (void)dealloc {
 	[name release];
 	[services release];
+	[sessions release];
 	[super dealloc];
 }
 
